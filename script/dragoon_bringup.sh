3427ad44e2df4385582faf385c0af11666a50de5
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-03-15 15:15:14 -0400

    add carto launch and seek link in urdf

--- script/dragoon_bringup.sh
+++ script/dragoon_bringup.sh
@@ -1,9 +1,7 @@
-source ../../../devel/setup.bash
 gnome-terminal --tab -- roscore
-sleep 1
+sleep 3
 gnome-terminal --tab -- roslaunch dragoon_bringup dragoon_bringup.launch
 sleep 4
-source ~/test_ws/devel/setup.bash
 gnome-terminal --tab -- roslaunch darknet_ros yolo_v3.launch
 sleep 5
 gnome-terminal --window -- jtop
