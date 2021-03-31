830bf95ab9fd566c70c5d4d258bccee25791a38d
Ben Kolligs <bkolligs@andrew.cmu.edu>
2021-03-31 11:15:01 -0400

    edit urdf after hardware assembly

--- script/dragoon_bringup.sh
+++ script/dragoon_bringup.sh
@@ -2,6 +2,6 @@ gnome-terminal --tab -- roscore
 sleep 3
 gnome-terminal --tab -- roslaunch dragoon_bringup dragoon_bringup.launch
 sleep 4
-gnome-terminal --tab -- roslaunch darknet_ros yolo_v3.launch
+gnome-terminal --tab -- roslaunch darknet_ros yolo_v3_thermal.launch
 sleep 5
 gnome-terminal --window -- jtop
